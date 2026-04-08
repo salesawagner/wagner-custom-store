1b00bf8c879e090f9c2906ed796ead3b0c3d36be
Wagner Sales <salesawagner@gmail.com>
2026-04-08 14:11:35 -0300

    Add whisparr-v3 and whisparr fixes

--- salesawagner-whisparr-v3/exports.sh
+++ salesawagner-whisparr-v3/exports.sh
@@ -2,12 +2,12 @@
 installed_apps=$("${UMBREL_ROOT}/scripts/app" ls-installed)
 
 if echo "$installed_apps" | grep --quiet 'qbittorrent'; then
-  export APP_WHISPARR_QBITTORRENT_INSTALLED="true"
+  export APP_WHISPARR_V3_QBITTORRENT_INSTALLED="true"
 fi
 
 if echo "$installed_apps" | grep --quiet 'sabnzbd'; then
-  export APP_WHISPARR_SABNZBD_INSTALLED="true"
+  export APP_WHISPARR_V3_SABNZBD_INSTALLED="true"
   # export SABNZBD_API_KEY, which has the format:
   # api_key = 98e3444f7fab45e592958673bf656g3
-  export APP_WHISPARR_SABNZBD_API_KEY=$(grep -Po 'api_key = \K.*' "${UMBREL_ROOT}/app-data/sabnzbd/data/config/sabnzbd.ini")
+  export APP_WHISPARR_V3_SABNZBD_API_KEY=$(grep -Po 'api_key = \K.*' "${UMBREL_ROOT}/app-data/sabnzbd/data/config/sabnzbd.ini")
 fi
